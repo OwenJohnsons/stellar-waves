90b11f9b9a2680c8f75601f27fc0a9ec6b548b94
Owen Johnson <60224822+OwenJohnsons@users.noreply.github.com>
2024-05-16 09:56:17 +0100

    Working version of filterbank generation.

--- sw-filgen.sh
+++ sw-filgen.sh
@@ -6,13 +6,21 @@ zst_files=(${path}/*.zst)
 start_time=$(echo "${zst_files[0]}" | awk -F '[/_T]' '{print $9}' | sed 's/..$//')
 date=$(echo "${zst_files[0]}" | cut -d'/' -f8 | cut -d'.' -f3 | cut -dT -f1)
 time=$(echo "${zst_files[0]}" | cut -d'/' -f8 | cut -dT -f2 | cut -d'.' -f1)
-formatted_date=$(echo $start_time | sed 's/\(....\)\(..\)\(..\)\(..\)\(..\)/\1:\2:\3_\4:\5:/; s/\(.*\):/\1\./')
+formatted_date=$(echo $start_time | sed 's/\(....\)\(..\)\(..\)\(..\)\(..\)/\1-\2-\3T\4:\5:/; s/\(.*\):/\1\:/')
+RA=$(python -c "from astropy.coordinates import SkyCoord; c = SkyCoord.from_name('$source'); print(c.ra.to_string(unit='hourangle', sep=':', precision=2))")
+DEC=$(python -c "from astropy.coordinates import SkyCoord; c = SkyCoord.from_name('$source'); print(c.dec.to_string(unit='degree', sep=':', precision=2))")
+tstartmjd=$(python -c "from astropy.time import Time; print(Time('$formatted_date').mjd)" )
+
 
 echo " =========== Processing filterbanks for $source =========== "
 echo " Path: $path"
 echo " Number of .zst files: $(ls -1a "${path}"/udp*.zst | wc -l)"
 echo " Obs Date: $date"
 echo " Obs Time: $time"
+echo " RA: $RA"
+echo " DEC: $DEC"
+echo " MJD Start: $tstartmjd"
+echo " =========================================================== "
 
 datetime="${obs_date} ${obs_time}"
 
@@ -26,32 +34,23 @@ else
     echo "No matching pattern found in the string."
 fi
 
-lofar_udp_extractor -i ${modified_string} -u 4 -p 154 -o "/mnt/ucc4_data2/data/Owen/raw-radio-stars/$source_"_"$formatted_date"_S%d.dada | tee -a ./logs/filgen_output_"$source_"_"$formatted_date".log
-grep "Start time" ./logs/filgen_output_"$source_"_"$formatted_date".log | awk '{print $3}' > ./logs/start_time_"$source_"_"$formatted_date".log
-tstart_true=$(cat ./logs/start_time_"$source_"_"$formatted_date".log | head -n 1)
+lofar_udp_extractor \
+    -i "${modified_string}" \
+    -u 4 \
+    -p 154 \
+    -a "-fch1 200 -fo -0.1953125 -source ${source_} -ra ${RA} -dec ${DEC}" \
+    -o "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_raw_S%d.fil" | tee -a "./logs/filgen_output_${source_}_${formatted_date}.log"
 
-# 4 pol, 488 beams, 195312.5 samples per second -> 23828125 bytes per second due to 16x time scrunch 
-bytes=$(echo $tstart_true | awk -F. '{print "0."$2}' | awk '{printf("%d\n", $1 * 23828125) }')
-echo " Bytes: $bytes"
-
-# # --- Header Manipulation ---
+# # --- Fil Generation ---
 for i in {0..3}; do
-    hdr_loc="/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_S${i}.hdr"
-    cp ./hdrtemplate.hdr "$hdr_loc"
-    date=$(echo "$tstart_true" | awk -F'T' '{print $1}')
-    time=$(echo "$tstart_true" | awk -F'T' '{print $2}' | awk -F. '{print $1}')
-    tstartmjd=$(echo "$tstart_true" | python -c "from astropy.time import Time; import sys; time = sys.stdin.readline().strip(); print(Time(time).mjd)" )
-    RA=$(python -c "from astropy.coordinates import SkyCoord; c = SkyCoord.from_name('$source'); print(c.ra.to_string(unit='hourangle', sep=':', precision=2))")
-    DEC=$(python -c "from astropy.coordinates import SkyCoord; c = SkyCoord.from_name('$source'); print(c.dec.to_string(unit='degree', sep=':', precision=2))")
-    sed -i "s|isotstart|${date}-${time}|g" "$hdr_loc"
-    sed -i "s|isooffset|${bytes}|g" "$hdr_loc"
-    sed -i "s|src_name|${source}|g" "$hdr_loc"
-    sed -i "s|racoords|${RA}|g" "$hdr_loc"
-    sed -i "s|decoords|${DEC}|g" "$hdr_loc"
+    if [ ! -f "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_S${i}.fil" ]; then
+        digifil -b-32 -t 128 -c -I 0 "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_raw_S${i}.fil" \
+        -o "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_S${i}.fil"
+        chmod 777 "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_S${i}.fil"
+    else
+        echo "File already exists: /mnt/ucc4_data2/data/Owen/raw-radio-stars/${source_}_${formatted_date}_S${i}.fil skipping digifil"
+    fi
 done
 
-
-# --- Fil Generation ---
-for i in {0..3}; do
-    digifil -b 8 -t 128 "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source}_${formatted_date}_S${i}.dada" -o "/mnt/ucc4_data2/data/Owen/raw-radio-stars/${source}_${formatted_date}_S${i}.fil"
-done
+# --- Clean Up ---
+rm -f /mnt/ucc4_data2/data/Owen/raw-radio-stars/${source}_${formatted_date}_raw_S*.fil
